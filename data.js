4f811cb63c4290db554fb27e66d7d775bb482a77
Jojo101303 <144183092+Jojo101303@users.noreply.github.com>
2024-05-10 02:27:57 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,84 @@
+export const mapClasses = [
+  "variant-1",
+  "variant-2",
+  "variant-3",
+  "variant-1",
+  "variant-2",
+  "variant-3",
+  "variant-1",
+  "variant-2",
+  "variant-3",
+  "variant-1",
+];
+export const previews = [
+  {
+    img: "./assets/main-1.jpg",
+    title: "Minimalist",
+    tags: "less is more",
+    description:
+      "Exploring the intersection of minimalism and future lifestle",
+  },
+  {
+    img: "./assets/main-2.jpg",
+    title: "Maximalist",
+    tags: "Fill up the space with intersection and complexity",
+    description:
+      "Innovative way to stay fulfilled and the elegance of handling.",
+  },
+  {
+    img: "./assets/main-3.jpg",
+    title: "Industrial",
+    tags: "I work even at home",
+    description:
+      "An attitude of precision, efficiency, and resistance",
+  },
+  {
+    img: "./assets/main-4.jpg",
+    title: "Bohemian",
+    tags: "Home should be as warm as the sunlight",
+    description:
+      "the light yellow shinning throw the window, depicting the warthm of family",
+  },
+  {
+    img: "./assets/main-5.jpg",
+    title: "Art Deco",
+    tags: "the soft but straighforward days",
+    description:
+      "enjoy the empty spaces with geometric shapes that are designed for your body",
+  },
+  {
+    img: "./assets/main-6.jpg",
+    title: "Traditional",
+    tags: "time froze in my house",
+    description:
+      "the past can be the present",
+  },
+  {
+    img: "./assets/main-7.jpg",
+    title: "Art Nouveau",
+    tags: "living in a garden",
+    description:
+      "the entangling vines lets me see nature and my mind",
+  },
+  {
+    img: "./assets/main-8.jpg",
+    title: "Modernism",
+    tags: "the soft but straighforward days",
+    description:
+      "enjoy the empty spaces with geometric shapes that are designed for your body",
+  },
+  {
+    img: "./assets/main-9.jpg",
+    title: "Post-Modernism",
+    tags: "I want everything everywhere all at once",
+    description:
+      "Paradise for creativity",
+  },
+  {
+    img: "./assets/main-10.jpg",
+    title: "All Work",
+    tags: "Minimalist Aesthetics, Fashion Forward",
+    description:
+      "Fashion forward web design encapsulating the essence of minimalist aesthetics and innovation.",
+  },
+];
